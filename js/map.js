249c4d7ec8db74cf05714a4335d39569d63ccda0
Ilya <mordar14@yandex.ru>
2018-11-30 20:13:44 +0400

    Доделал задание. Теперь можно просматривать объявления

--- js/map.js
+++ js/map.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var INCREASE_INDEX = 1;
 var ADVERTISING_COUNT = 8;
 var MIN_PRICE = 1000;
 var MAX_PRICE = 1000000;
@@ -21,25 +20,26 @@ var checkinsCheckouts = ['12:00', '13:00', '14:00'];
 
 var pathsPhotos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
-//Генерируем случайное число
+//  Генерируем случайное число
 var getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-//Генерируем случайный элемент массива
+//  Генерируем случайный элемент массива
 var getRandomIndexElement = function (array) {
   return array[Math.floor(Math.random() * array.length)];
 };
 
-//Генерируем случайную длинну
+//  Генерируем случайную длинну
 var getRandomLength = function (array) {
   return array.slice(getRandomNumber(0, array.length - 1));
 };
 
-var generateAdvert = function(advertsCount) {
+var generateAdvert = function (advertsCount) {
   var manyAdverts = [];
 
-  //Генерируем аватар пользователя
+  // Генерируем аватар пользователя
+
   var getAvatarUser = function (indexAvatarImage) {
     var auxiliaryIndex = indexAvatarImage + 1;
     return 'img/avatars/user0' + auxiliaryIndex + '.png';
@@ -74,18 +74,18 @@ var generateAdvert = function(advertsCount) {
     };
     manyAdverts.push(oneAdvert);
   }
-  return  manyAdverts;
+  return manyAdverts;
 };
 
 
-//Отрисуем метки
+//  Отрисуем метки
 var manyAdverts = generateAdvert(ADVERTISING_COUNT);
 
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
 var pinElements = document.querySelector('.map__pins');
 
-var getPin = function(charactersPin) {
+var getPin = function (charactersPin) {
   var pinOneElement = pinTemplate.cloneNode(true);
   pinOneElement.style = 'left:' + charactersPin.location.x + 'px;' + 'top:' + charactersPin.location.y + 'px;';
   pinOneElement.querySelector('img').src = charactersPin.author.avatar;
@@ -93,7 +93,7 @@ var getPin = function(charactersPin) {
   return pinOneElement;
 };
 
-var getPinFragment = function() {
+var getPinFragment = function () {
   var pinFragment = document.createDocumentFragment();
   for (var i = 0; i < manyAdverts.length; i++) {
     pinFragment.appendChild(getPin(manyAdverts[i]));
@@ -106,20 +106,32 @@ getPinFragment();
 
 // Отрисуем объявления
 
-var getPhotos = function(photosArr) {
+var getPhotos = function (photosArr) {
   var photosFragment = document.createDocumentFragment();
-  var photosTemplate = document.querySelector('#card')
-  .content
-  .querySelector('.popup__photos');
-  var photosOneElement = photosTemplate.cloneNode(true);
-  for (var i = 0; i < photosArr; i++) {
+  for (var i = 0; i < photosArr.length; i++) {
+    var photosTemplate = document.querySelector('#card')
+    .content
+    .querySelector('.popup__photos');
+    var photosOneElement = photosTemplate.cloneNode(true);
     photosOneElement.querySelector('img').src = photosArr[i];
+    photosOneElement.querySelector('img').width = 60;
+    photosOneElement.querySelector('img').height = 60;
     photosFragment.appendChild(photosOneElement);
   }
   return photosFragment;
 };
 
-var getCard = function(charactersCard) {
+var getFeatures = function (featuresArr) {
+  var featuresFragment = document.createDocumentFragment();
+  for (var i = 0; i < featuresArr.length; i++) {
+    var featuresOneElement = document.createElement('li');
+    featuresOneElement.className = 'popup__feature popup__feature--' + featuresArr[i];
+    featuresFragment.appendChild(featuresOneElement);
+  }
+  return featuresFragment;
+};
+
+var getCard = function (charactersCard) {
   var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
   var cardOneElement = cardTemplate.cloneNode(true);
   cardOneElement.querySelector('.popup__avatar').src = charactersCard.author.avatar;
@@ -129,16 +141,18 @@ var getCard = function(charactersCard) {
   cardOneElement.querySelector('.popup__type').textContent = charactersCard.offer.type;
   cardOneElement.querySelector('.popup__text--capacity').textContent = charactersCard.offer.rooms + 'комнаты для ' + charactersCard.offer.rooms + 'гостей';
   cardOneElement.querySelector('.popup__text--time').textContent = 'Заезд до ' + charactersCard.offer.checkin + ', выезд после ' + charactersCard.offer.checkout;
+  cardOneElement.querySelector('.popup__features').textContent = '';
+  cardOneElement.querySelector('.popup__features').appendChild(getFeatures(charactersCard.offer.features));
   cardOneElement.querySelector('.popup__description').textContent = charactersCard.offer.description;
-  cardOneElement.querySelector('.popup__photos').appendChild(getPhotos(charactersCard.offer.photos));
   cardOneElement.querySelector('.popup__photos').textContent = '';
+  cardOneElement.querySelector('.popup__photos').appendChild(getPhotos(charactersCard.offer.photos));
   return cardOneElement;
 };
 
-var getCardFragment = function() {
+var getCardFragment = function () {
   var cardFragment = document.createDocumentFragment();
   cardFragment.appendChild(getCard(manyAdverts[2]));
   document.querySelector('.map').insertBefore(cardFragment, document.querySelector('.map__filter-container'));
 };
 
- getCardFragment();
+getCardFragment();
